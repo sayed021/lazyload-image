7eb3ed5623a5886e3fb311ec239a40fd2542b9be
sayedbs <sayeed@bs-23.net>
2022-07-27 11:48:30 +0600

    es6 to es5 and others improvment

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,24 @@
-// create config object: rootMargin and threshold
-// are two properties exposed by the interface
-const config = {
-    rootMargin: '5px 0px 50px 0px',
-    threshold: 0
-};
+// threshold allow 0 to 1, and this mean 0% to 100% element on viewport 
+// rootMargin mean the container section make it larger rootMargin value or make smaller using Negetive mergin ( ex: -50px )
+const config = { rootMargin: '20px 0px 20px 0px', threshold: 0 };
 
 let preloadImage = function(img) {
-    img.setAttribute('src', img.getAttribute('data-src')).addClass('loaded');
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.classList.add('loaded')
     return img 
-}
+};
 
-// register the config object with an instance
-// of intersectionObserver
 let observer = new IntersectionObserver(function(entries, self) {
-    // iterate over each entry
     entries.forEach(function(entry) {
-        // process just the images that are intersecting.
-        // isIntersecting is a property exposed by the interface
         if(entry.isIntersecting) {
-            // custom function that copies the path to the img
-            // from data-lazy to src
             preloadImage(entry.target);
-            // the image is now in place, stop watching
             self.unobserve(entry.target);
         }
     });
 }, config);
 
-
-
-window.addEventListener('load', (event) => {
-    const imgs = document.querySelectorAll('[data-src]');
-    imgs.forEach(img => {
+window.addEventListener('load', function() {
+    document.querySelectorAll('[data-src]').forEach(function(img) {
         observer.observe(img);
     });
 });
